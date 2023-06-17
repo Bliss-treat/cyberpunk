7ee91fd396a67167319cb4d0db913034df464759
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-17 20:32:05 +0300

    added hello world.c and its executable file

---
+++ c-projects/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success) 
+ */
+
+int main(void)
+{
+printf("Just started programming in c\n");
+return (0);
+}
